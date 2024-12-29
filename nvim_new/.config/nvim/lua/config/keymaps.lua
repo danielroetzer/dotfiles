5c05551169c81ff52ff5872b6c67f0113819534f
Daniel Rötzer <52409858+danielroetzer@users.noreply.github.com>
2024-12-29 17:48:51 +0100

    update default config and keymaps

--- nvim_new/.config/nvim/lua/config/keymaps.lua
+++ nvim_new/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("i", "jk", "<Esc>")
+
+vim.keymap.set("", "<C-f>", "20jzz")
+vim.keymap.set("", "<C-b>", "20kzz")
+vim.keymap.set("", "<C-d>", "12jzz")
+vim.keymap.set("", "<C-u>", "12kzz")
