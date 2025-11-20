7439193a7f5dd4e42ada8dbc10696dd3340b29bd
Daniel Rötzer <52409858+danielroetzer@users.noreply.github.com>
2025-11-20 11:02:49 +0100

    fix wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,8 +4,8 @@ local config = wezterm.config_builder()
 config.color_scheme = "Catppuccin Mocha"
 config.font_size = 14
 
--- hide_tab_bar_if_only_one_tab = true
-enable_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+-- config.enable_tab_bar = false
 
 -- config.window_background_opacity = 0.7
 
